98bf5649c66271d90e7dc850bfac9ff0d0cafffc
Lexi4eg <felix.prattes@gmail.com>
2023-02-08 13:06:32 +0100

    Homepage v1..01

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="flex flex-row justify-between p-4">
+      <div>Automat</div>
+      <div>Gruppe Blau</div>
+    </div>
+  );
+}
+
+export default Navbar;
