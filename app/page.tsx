ba01ec3bc014042016318c8970ef853750a66a20
Felix Prattes <74205407+Lexi4eg@users.noreply.github.com>
2023-04-04 12:43:58 +0200

    new version

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,21 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-6 grid-rows-6 w-screen h-screen">
           <div className="col-span-2 row-span-2 items-center">
-            <div className="text-white text-4xl w-60 ">
-              Productive Free and easy new
+            <div className=" flex items-center h-full w-full justify-center ">
+              <div className="text-white justify-items-center">
+                <div className="text-5xl">Productive</div>
+                <div className="text-5xl">Free and easy</div>
+                <div className="text-5xl">New.</div>
+              </div>
             </div>
           </div>
           <div className="col-span-4 row-span-5 bg-white"></div>
-          <div className="col-span-2 row-span-3 bg-slate-100"></div>
+          <div className="col-span-2 row-span-3 bg-slate-100">
+            <div className="flex flex-col items-center">
+              <Image src={"/1.png"} width={450} height={800} alt={""} />
+              <div className="text-7xl text-center">Productive</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
